f7c74f64b979389d0c755086eaeec106be2a0483
Ascent817 <vapradeep817@gmail.com>
2022-12-25 19:39:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-const fs = require('fs');
-const http = require('http');
+import { Server } from "socket.io";
 
-const express = require('express');
-const app = express();
-
-const options = {
-  cors: {
-    origin: "https://master--dainty-dusk-3f3151.netlify.app"
-  }
-};
-
-var serverPort = 3000;
-
-var server = http.createServer(options, app);
-var io = require('socket.io')(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/payload.html');
+const io = new Server({
+    cors: {
+        origin: "*"
+    }
 });
 
-io.on('connection', function(socket) {
-  console.log('new connection');
+io.on("connection", (socket) => {
+    console.log(socket.id);
+    
+    socket.on("disconnect", (reason) => {
+        console.log(`reason: ${reason}`);
+    });
 });
 
-io.on('post', (input) => {
-  console.log(input);
-  io.emit("update", input);
-});
-
-server.listen(serverPort, "0.0.0.0");
+io.listen(3000);
